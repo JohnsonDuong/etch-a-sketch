204f8023c971c9766b95bfd6035184c98cc0e5bb
Johnson Duong <71087545+johnsonduong@users.noreply.github.com>
2022-05-16 15:15:40 -0400

    Add drawing functionality only when mouse is clicked

--- app.js
+++ app.js
@@ -3,9 +3,27 @@
 //Variables
 
 //Elements selected from the DOM
-const grid = document.getElementById('grid-container');
+const DEFAULT_MODE = 'color';
+const DEFAULT_COLOR = 'white';
+const DEFAULT_SIZE = '16';
 
+const grid = document.getElementById('grid-container');
+let currentMode = DEFAULT_COLOR;
+let mouseDown = false;
+
+document.getElementById('grid-container').onmousedown = () => mouseDown = true;
+document.getElementById('grid-container').onmouseup = () => mouseDown = false;
+
+ 
 function changeColor(e) {
+
+    console.log(e.type);
+    console.log(mouseDown);
+    if (e.type === 'mouseover' && !mouseDown) return;
+
+    // if () {
+
+    // }
     e.target.style.backgroundColor = 'green';
 }
 
@@ -17,10 +35,10 @@ function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const gridPixel = document.createElement('div');
         gridPixel.classList.add("grid-pixel");
-        gridPixel.addEventListener('mouseover', changeColor);
         gridPixel.addEventListener('mousedown', changeColor);
+        gridPixel.addEventListener('mouseover', changeColor);
         grid.appendChild(gridPixel);
       }
-}
+}   
 
 createGrid(20);
