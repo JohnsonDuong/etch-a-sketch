602bb577d354c91271517f23f965681988054424
Johnson Duong <71087545+johnsonduong@users.noreply.github.com>
2022-05-19 20:23:58 -0400

    Add ability to change grid size using a slider

--- app.js
+++ app.js
@@ -4,42 +4,51 @@
 
 //Elements selected from the DOM
 const DEFAULT_MODE = 'color';
-const DEFAULT_SIZE = '50';
-const BACKGROUND_COLOR = 'white';
+const DEFAULT_SIZE = '16';
 
+//DOM elements
 const grid = document.getElementById('grid-container');
 const colorButton = document.getElementById('color-button');
 const rainbowButton = document.getElementById('rainbow-button');
 const eraserButton = document.getElementById('eraser-button');
 const toggleGridButton = document.getElementById('grid-button');
 const clearButton = document.getElementById('clear-button');
-
+const bgColorPicker = document.getElementById('background-color-picker');
+const slider = document.getElementById('slider');
 
 //Variables
 let currentMode = DEFAULT_MODE;
 let currentSize = DEFAULT_SIZE;
 let gridLineMode = false;
 
+//Event listeners
+bgColorPicker.oninput = () => grid.style.backgroundColor = bgColorPicker.value;
 colorButton.onclick = () => setCurrentMode('color');
 rainbowButton.onclick = () => setCurrentMode('rainbow');
 eraserButton.onclick = () => setCurrentMode('eraser');
+toggleGridButton.onclick = () => toggleGrid();
 
+slider.oninput = () => createGrid(slider.value);
 
+let mouseDown = false;
+document.getElementById('grid-container').onmousedown = () => mouseDown = true;
+document.getElementById('grid-container').onmouseup = () => mouseDown = false;
 
 //Clear the canvas
-clearButton.onclick = () => {
+clearButton.onclick = () => clearCanvas();
 
+function clearCanvas() {
     for (const pixel of pixelList) {
         pixel.classList.remove('filled-pixel');
-        pixel.style.backgroundColor = BACKGROUND_COLOR;
+        pixel.style.background = 'none';
     }
-
 }
 
 //Toggle the grid lines
-toggleGridButton.onclick = () => {
-    gridLineMode = !gridLineMode;
+function toggleGrid () {
     
+    gridLineMode = !gridLineMode;
+
     //Show the grid lines
     if (gridLineMode == true){
         pixelList.forEach((pixel) => pixel.classList.add('grid-lines'));
@@ -49,12 +58,9 @@ toggleGridButton.onclick = () => {
     else if (gridLineMode == false){
         pixelList.forEach((pixel) => pixel.classList.remove('grid-lines'));
     }
-};
+}
 
-let mouseDown = false;
-document.getElementById('grid-container').onmousedown = () => mouseDown = true;
-document.getElementById('grid-container').onmouseup = () => mouseDown = false;
-
+//Set the current mode
 function setCurrentMode (newMode) {
     currentMode = newMode;
 }
@@ -69,7 +75,7 @@ function draw(e) {
 
     //Set the brush to color mode
     if (currentMode == 'color') {
-        let currentColor = document.getElementById('color-picker').value;
+        let currentColor = document.getElementById('pen-color-picker').value;
         pixel.style.backgroundColor = currentColor;
         pixel.classList.add('filled-pixel');
         // console.log(pixel)
@@ -87,26 +93,36 @@ function draw(e) {
 
     //Toggle the eraser
     else if (currentMode == 'eraser') {
-        pixel.style.backgroundColor = 'white';
+        pixel.style.background = 'none';
         pixel.classList.remove('filled-pixel');
     }
 }
 
 //Create the grid
 function createGrid(size) {
+
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
+    //Remove any exisiting grid pixels
+    while (grid.hasChildNodes()) {
+        grid.removeChild(grid.firstChild);
+    }
+
     for (let i = 0; i < size * size; i++) {
         const gridPixel = document.createElement('div');
-        // gridPixel.classList.add("grid-pixel");
+        
+        if (gridLineMode) {
+            gridPixel.classList.add("grid-lines");
+        }
+        
         gridPixel.addEventListener('mousedown', draw);
         gridPixel.addEventListener('mouseover', draw);
         grid.appendChild(gridPixel);
-      }
+    }
+
 }   
 
 createGrid(currentSize);
-
 
 let pixelList = grid.childNodes;
